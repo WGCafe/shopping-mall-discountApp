9f5c6968b8af114ad82ac8efb05cd9b11836d239
wendylin <wendylw2013@gmail.com>
2021-05-23 12:54:11 +0800

    Update page header for detail page

--- src/pages/storeOrMallDetail/index.tsx
+++ src/pages/storeOrMallDetail/index.tsx
@@ -7,9 +7,41 @@ import UseNavInfo from '../../components/useNavInfo';
 
 const Index = () => {
   const { statusBarHeight, appHeaderHeight, titelBarWidth, marginSides } = UseNavInfo();
+  const handelClickBackButton = () => {
+    Taro.navigateBack({delta: 1});
+  };
 
   return (
     <View>
+      <View
+        style={{
+          position: 'fixed',
+          backgroundColor: '#fff',
+          zIndex: 1,
+          width: '100vw',
+          boxSizing: 'border-box',
+          paddingTop: statusBarHeight + (marginSides / 2),
+          minHeight: appHeaderHeight,
+        }}
+      >
+        <View
+          className='at-row at-row__align--center'
+          style={{
+            boxSizing: 'border-box',
+            width: titelBarWidth,
+          }}
+        >
+          <AtIcon
+            className='at-col at-col-1 at-col--auto'
+            customStyle={{
+              verticalAlign: 'middle',
+              padding: marginSides,
+            }}
+            prefixClass='iconfont' value='back'
+            onClick={handelClickBackButton}
+          />
+        </View>
+      </View>
     </View>
   )
 }

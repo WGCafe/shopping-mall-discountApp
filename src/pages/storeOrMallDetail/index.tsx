c2be3a5b2256406eb4e9a6dba0264e7890988fe4
wendylin <wendylw2013@gmail.com>
2021-05-30 14:49:17 +0800

    Update detail ui

--- src/pages/storeOrMallDetail/index.tsx
+++ src/pages/storeOrMallDetail/index.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import { View, Image, Text } from '@tarojs/components'
 import { AtIcon } from 'taro-ui';
 import Taro from '@tarojs/taro';
-
 import UseNavInfo from '../../components/useNavInfo';
+import './index.styl';
 
 const Index = () => {
   const { statusBarHeight, appHeaderHeight, titelBarWidth, marginSides } = UseNavInfo();
@@ -42,59 +42,81 @@ const Index = () => {
           />
         </View>
       </View>
-      <View>
-        <Image src="https://blog.viewneo.com/wp-content/uploads/2020/07/shopping-mall-with-3-floors.jpg" />
-        <Text>米兰 女神节洗剪吹</Text>
-      </View>
-      <View>
-        <View>
-          <AtIcon
-            className="detail__icon"
-            customStyle={{
-              padding: '0.1rem',
-              fontSize: '1rem'
-            }}
-            prefixClass='iconfont' value='time'
-          />
-          <Text className='detail__time'>10:00～22:00</Text>
-        </View>
-        <View>
-          <AtIcon
-            className="detail__icon"
-            customStyle={{
-              padding: '0.1rem',
-              fontSize: '1rem'
-            }}
-            prefixClass='iconfont' value='car'
-          />
-          <Text className='detail__time'>可停车</Text>
-        </View>
-        <View>
-          <AtIcon
-            className="detail__icon"
-            customStyle={{
-              padding: '0.1rem',
-              fontSize: '1rem'
-            }}
-            prefixClass='iconfont' value='wifi'
-          />
-          <Text className='detail__time'>Wi-Fi</Text>
-        </View>
-      </View>
 
-      <View>
-        <AtIcon
-          className="search__item-icon"
-          customStyle={{
-            fontSize: '1.2rem'
-          }}
-          prefixClass='iconfont' value='location_light'
-        />
-        <Text className='search__item-description'>[南浦大桥/陆家浜路]</Text>
-      </View>
+      <View style={{
+          paddingTop: appHeaderHeight,
+        }}>
+        <View className="detail__image-container">
+          <Image src="https://blog.viewneo.com/wp-content/uploads/2020/07/shopping-mall-with-3-floors.jpg" />
+        </View>
+        <Text className="detail__title">米兰 女神节洗剪吹</Text>
+        <View className="detail__info-list-container">
+          <View className="detail__info-list">
+            <View className="detail__info">
+              <AtIcon
+                className="detail__info-icon"
+                customStyle={{
+                  padding: '0.2rem',
+                  fontSize: '1.2rem'
+                }}
+                prefixClass='iconfont' value='time'
+              />
+              <Text className='detail__info-text'>10:00～22:00</Text>
+            </View>
+            <View className="detail__info">
+              <AtIcon
+                className="detail__info-icon"
+                customStyle={{
+                  padding: '0.2rem',
+                  fontSize: '1.2rem'
+                }}
+                prefixClass='iconfont' value='car'
+              />
+              <Text className='detail__info-text'>可停车</Text>
+            </View>
+            <View className="detail__info">
+              <AtIcon
+                className="detail__info-icon"
+                customStyle={{
+                  padding: '0.2rem 0.4rem',
+                  fontSize: '1.2rem'
+                }}
+                prefixClass='iconfont' value='wifi'
+              />
+              <Text className='detail__info-text'>Wi-Fi</Text>
+            </View>
+          </View>
+        </View>
 
-      <View>
+        <View className="detail__info-description-container">
+          <View className="detail__info-description-list at-row">
+            <View className="at-col">
+              <AtIcon
+                className="detail__info-icon"
+                customStyle={{
+                  verticalAlign: 'top',
+                  padding: '0.2rem',
+                  fontSize: '1.4rem',
+                }}
+                prefixClass='iconfont' value='location_light'
+              />
+              <View className='detail__info-description'>
+                <Text className='detail__info-description-title'>[南浦大桥/陆家浜路]</Text>
+                <Text className='detail__info-description-hint'>距离你200m</Text>
+              </View>
+            </View>
 
+            <AtIcon
+              className="at-col at-col-1 at-col--auto"
+              customStyle={{}}
+              prefixClass='iconfont' value='phone_light'
+            />
+          </View>
+        </View>
+
+        <View>
+
+        </View>
       </View>
     </View>
   )
